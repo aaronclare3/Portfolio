c7494155797f876f264650699f070c2eb3bd4e7c
aaronclare3 <aaronclare88@gmail.com>
2021-03-01 14:20:58 -0800

    changed navbar back to outside of grid

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <>
+      <div className='About'>
+        <div className='pic'></div>
+        <div className='bio'></div>
+        <div className='skills'></div>
+      </div>
+    </>
+  );
+};
+
+export default About;
